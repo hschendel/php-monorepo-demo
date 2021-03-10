427312b0d3620896e97453ac925ff6eb832beeec
Hagen Schendel <mail@hschendel.de>
2021-03-10 23:18:55 +0100

    b_client: offers()

--- b_client/src/Client/BClient.php
+++ b_client/src/Client/BClient.php
@@ -10,16 +10,18 @@ class BClient
 {
     private $httpClient;
     private $baseURL;
+    private $json;
 
     public function __construct(HttpClientInterface $httpClient, string $baseURL) {
         $this->httpClient = $httpClient;
         $this->baseURL = $baseURL;
+        $this->json = new Json();
     }
 
     public function offers(): array {
         // TODO error handling
         $url = sprintf("%s/offers", $this->baseURL);
         $resp = $this->httpClient->request('GET', $url);
-
+        return $this->json->deserializeOffers($resp->getContent());
     }
 }
